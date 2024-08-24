a102e7f7154c1656a2874c7764466a9f255aab8c
ArpitaSahu <60890147+arpitasahu@users.noreply.github.com>
2024-08-25 01:25:23 +0530

    Delete Revising-Aggregations - The-Count-Function.sql

--- Revising-Aggregations - The-Count-Function.sql
+++
@@ -1,7 +0,0 @@
--- Revising Aggregations - The Count Function
--- https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem
-
-SELECT COUNT(NAME)
-FROM CITY
-WHERE POPULATION > 100000;
-
