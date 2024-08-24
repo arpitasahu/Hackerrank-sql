7c3f4e54dc2faa5ab180c4919e772d869d6070be
ArpitaSahu <60890147+arpitasahu@users.noreply.github.com>
2024-08-25 05:27:57 +0530

    Add files via upload

---
+++ Aggregation/revising-aggregations-the-average-function.sql
@@ -0,0 +1,6 @@
+-- revising-aggregations-the-average-function
+-- https://www.hackerrank.com/challenges/revising-aggregations-the-average-function
+
+SELECT AVG(POPULATION)
+FROM CITY
+WHERE DISTRICT = 'California';
