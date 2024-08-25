7a3c39059825427dd1af6433cb5622c8ae8fdcc6
ArpitaSahu <60890147+arpitasahu@users.noreply.github.com>
2024-08-25 05:58:41 +0530

    Delete average-population-of-each-continent.sql

--- average-population-of-each-continent.sql
+++
@@ -1,7 +0,0 @@
--- average-population-of-each-continent
--- https://www.hackerrank.com/challenges/average-population-of-each-continent
-
- SELECT COUNTRY.CONTINENT, FLOOR(AVG(CITY.POPULATION))
-FROM COUNTRY
-JOIN CITY ON COUNTRY.CODE = CITY.COUNTRYCODE
-GROUP BY COUNTRY.CONTINENT; 
